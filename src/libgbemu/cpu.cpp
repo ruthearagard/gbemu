6ddae480b070a894a17dc92ae78523d414508124
Michael Rodriguez <ruthearagard@gmail.com>
2020-08-20 08:40:10 -0400

    Implement the `LD C, (HL)` (0x4E) instruction.

--- src/libgbemu/cpu.cpp
+++ src/libgbemu/cpu.cpp
@@ -536,6 +536,13 @@ auto CPU::step() noexcept -> void
 
             return;
 
+        // LD C, (HL)
+        case 0x4E:
+            reg.c = m_bus.read(hl());
+            reg.pc++;
+
+            return;
+
         // LD (HL), A
         case 0x77:
             m_bus.write(hl(), reg.a);
