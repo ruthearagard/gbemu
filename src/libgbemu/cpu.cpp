ad3863c25830937a5331599e8b3c6a2e2e05f7cf
Michael Rodriguez <ruthearagard@gmail.com>
2020-08-20 11:22:20 -0400

    Implement the `LD H, (HL)` (0x66) instruction.

--- src/libgbemu/cpu.cpp
+++ src/libgbemu/cpu.cpp
@@ -804,6 +804,13 @@ auto CPU::step() noexcept -> void
 
             return;
 
+        // LD H, (HL)
+        case 0x66:
+            reg.h = m_bus.read(hl());
+            reg.pc++;
+
+            return;
+
         // LD H, A
         case 0x67:
             reg.h = reg.a;
